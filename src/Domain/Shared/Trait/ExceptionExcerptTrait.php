3ac78c9ee15cf119f3a9049b31bf2d4a6729789c
shamisen-cat <176907131+shamisen-cat@users.noreply.github.com>
2025-10-28 22:47:02 +0900

    feat: 例外メッセージで使用する値の短縮表示を生成するトレイト

---
+++ src/Domain/Shared/Trait/ExceptionExcerptTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Shared\Trait;
+
+/**
+ * 例外メッセージで使用する値の短縮表示を生成するトレイト
+ */
+trait ExceptionExcerptTrait
+{
+    /** 短縮表示の最大文字数 */
+    private const EXCERPT_LENGTH = 20;
+
+    /** 省略記号 */
+    private const EXCERPT_ELLIPSIS = '...';
+
+    /**
+     * 値の短縮表示を取得する。
+     *
+     * @param string $value 例外メッセージで使用する値
+     *
+     * @return string 値の短縮表示
+     */
+    private function getExcerpt(string $value): string
+    {
+        return mb_strlen($value) > self::EXCERPT_LENGTH
+            ? mb_substr($value, 0, self::EXCERPT_LENGTH).self::EXCERPT_ELLIPSIS
+            : $value;
+    }
+}
