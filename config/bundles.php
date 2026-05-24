068d0deb84edf9deff4d25a08ba71616b367f8e2
shamisen-cat <176907131+shamisen-cat@users.noreply.github.com>
2026-05-24 12:21:13 +0000

    chore: symfony/ux-icons を追加

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
+    Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
 ];
